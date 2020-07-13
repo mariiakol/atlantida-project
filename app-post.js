58cd3463af0fb1d1c85c455f7e1ba51b9ecf13f2
Mariia <maria.gorn17@gmail.com>
2020-07-13 11:49:04 +0300

    mobile menu

--- app-post.js
+++ app-post.js
@@ -1,9 +1,3 @@
-const selector = document.querySelectorAll('.post')
-console.log(selector);
-for (let i = 0; i < selector.length; i++){
-	selector[i].classList.add('magictime', 'vanishIn');
-}
-
 let mobileMenu = document.querySelector(".nav-mobile-menu");
 let mainMenu = document.querySelector(".navigation");
 
--- app.js
+++ app.js
@@ -20,7 +20,17 @@ wow = new WOW(
                     wow.init();
 
 
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".navigation");
 
+mobileMenu.addEventListener("click", function(){
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains('active-menu')) {
+        mainMenu.classList.add("active-menu")
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
+})
 
 
 
