7f6990cbc90b51a49f1516717b3213e49c22f217
Mariia <maria.gorn17@gmail.com>
2020-07-20 11:10:27 +0300

    tabs

--- app.js
+++ app.js
@@ -32,6 +32,21 @@ mobileMenu.addEventListener("click", function(){
     }
 })
 
+const tabLinks = document.querySelectorAll(".tabs a");
+const tabPanels = document.querySelectorAll(".tabs-panel");
 
+for (let el of tabLinks) {
+  el.addEventListener("click", e => {
+    e.preventDefault();
 
+    document.querySelector(".tabs li.active").classList.remove("active");
+    document.querySelector(".tabs-panel.active").classList.remove("active");
 
+    const parentListItem = el.parentElement;
+    parentListItem.classList.add("active");
+    const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+    const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
+    panel[0].classList.add("active");
+    });
+  }
